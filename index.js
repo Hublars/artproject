ef3de788164d3c1c92906e9538a725d504b8232a
Hublars <lars.fangstam@gmail.com>
2016-09-04 13:50:34 +0200

    Trying upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express'),
     request = require('request'),
     asynchr = require('async'),
     fs      = require('fs'),
+    clone   = require('clone'),
     app     = express(),
     konst   = require('./konst.json');
 
@@ -17,46 +18,88 @@ app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
 
-  // Collect addresses for the images in this array.
-  var imageAddresses = [];
-  for (var i = 0; i < konst.length; i++) {
+  // Uncomment this to dowload images.
+  //downloadImages();
 
-    for (var j = 0; j < konst[i].media.length; j++) {
-
-      imageAddresses.push(konst[i].media[j]);
+  var post_options = {
+    url: 'https://api.artworksapp.com/staging/artworks/',
+    headers: {
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1N2IzMDNiNTE4MjM2MDYyNGYwZGNmOWEiLCJpYXQiOjE0NzI3Mzg0Mjh9.LZ2IqfUKA6kSJnR2OYIiJ-OA6NdAJFV_se9Y22vDoz4',
+      'Content-Type': 'application/json'
+    },
+    json: {
+      title: 'Hello Art',
+      'user._id': '57b303b5182360624f0dcf9a',
+      images: [
+        {
+          src: '',
+          color: 'red',
+          width: 500,
+          height: 500
+        }
+      ]
     }
   }
 
-  var logit = true;
-
   var q = asynchr.queue(function(task, done) {
-    request(task.url, { encoding: 'binary' }, function(err, res, body) {
-      if (err) return done(err);
-      if (res.statusCode != 200) return done(res.statusCode);
 
-      var arr = task.url.split("/");
-      var imageName = arr[arr.length - 1];
-      var saveName = './images/' + imageName;
-      fs.writeFile(saveName, body, 'binary', function(err) {});
+    request.post(task.options, function(error, response, body) {
+
+      if (!error && response.statusCode == 200) {
+        console.log('Success!');
+        console.log(body);
+      }
+      else {
+        console.log('Error!');
+        console.log(body);
+      }
+
       done();
     });
   });
 
-  //Only trying a few images at a time.
-  //for (var i = 0; i < imageAddresses.length; i++) {
-  for (var i = 0; i < 3; i++) {
-    console.log(imageAddresses[i]);
-    q.push({ url: imageAddresses[i]});
+  //for (var i = 0; i < konst.length; i++) {
+  for (var i = 0; i < 3; i++) { // Trying a few at a time.
+    var p_options = clone(post_options);
+    p_options.json.title = konst[i].titel;
+    q.push({ options: p_options });
   }
 
-  res.send(imageAddresses);
-  //res.render('home');
+  //res.send(imageAddresses);
+  res.render('home');
 });
 
-app.get('/getimages', function (req, res) {
+function downloadImages() {
 
-  console.log('test');
-});
+    // Collect addresses for the images in this array.
+    var imageAddresses = [];
+    //for (var i = 0; i < konst.length; i++) {
+    for (var i = 0; i < 3; i++) { // Trying a few at a time.
+
+      for (var j = 0; j < konst[i].media.length; j++) {
+
+        imageAddresses.push(konst[i].media[j]);
+      }
+    }
+
+    // Download all images.
+    var q = asynchr.queue(function(task, done) {
+      request(task.url, { encoding: 'binary' }, function(err, res, body) {
+        if (err) return done(err);
+        if (res.statusCode != 200) return done(res.statusCode);
+
+        var arr = task.url.split("/");
+        var imageName = arr[arr.length - 1];
+        var saveName = './images/' + imageName;
+        fs.writeFile(saveName, body, 'binary', function(err) {});
+        done();
+      });
+    });
+
+    for (var i = 0; i < imageAddresses.length; i++) {
+      q.push({ url: imageAddresses[i]});
+    }
+}
 
 app.listen(3000, function() {
   console.log('App listening on port 3000');
